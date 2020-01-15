693e8357242f1071855200daab9aaeaab8c3f1eb
Devin Biddy <xp.gram@gmail.com>
2020-01-15 15:43:44 -0800

    add additional turn state-machine framework

--- src/scripts/battle/turn-machine/TurnState.ts
+++ src/scripts/battle/turn-machine/TurnState.ts
@@ -1,57 +1,54 @@
-import { Game } from "../../..";
-import { Debug } from "../../DebugUtils";
+import { TurnStateController } from "./TurnStateController";
 
 export abstract class TurnState {
-    /** All battle-scene-relevant objects, script controllers, and assets.
-     * Provides access to the MapCursor, the Map itself, the UI windows, etc. */
-    sceneAssets: BattleSceneObjectHandle;
+    /** Reference to the game state controller (battle). */
+    protected controller: TurnStateController;
 
-    constructor() {}
+    /** The name of this game state. Useful for debugging. */
+    abstract name: string;
 
-    /** Throws an error (great for debugging) if controllables' dependencies aren't met.
-     * Things like there not being a source and destination point defined during the
-     * unit-railcar-animation step, or worse Railcar.unitType not being defined. */
-    protected abstract assertDependencies(): void;
+    /** Whether this state refuses to execute when reached from a future state.
+     * If this setting is true, the desired behavior is to reach further back in state history when regressing. */
+    skipOnUndo = false;
+
+    constructor(cont: TurnStateController) {
+        this.controller = cont;
+    }
+
+    /** Returns false if this state's scene dependencies aren't met on state initiation.
+     * Useful for reporting when the previous state transitioned to next without providing the
+     * information next needs to operate. */
+    abstract assertDependencies(): boolean;
 
     /** Explicitly enables control scripts relevant to the state (important to avoid conflicts.)
      * ControlScripts not enabled here are necessarily disabled. */
     protected abstract configureScene(): void;
 
-    /** Probably won't need to do much, but still.
-     * Most control scripts will add themselves to the game.scene.ticker
-     * This can at least check for conditions for advancement to be met. */
-    protected abstract update(): void;
+    /** Any frame-by-frame tasks the state script needs to have done. Generally used to watch
+     * for state-advancement conditions. */
+    abstract update(): void;
 
-    /** Any to-dos before regressing to previous state.
-     * This should perform a complete 'undo' of whatever variables this state was trying to affect. */
-    protected abstract prev(): void;
+    /** Un-changes whatever scene information might have changed as a result of this state's control. */
+    abstract undo(): void;
 
-    init(assets: BattleSceneAssets) {
-        this.sceneAssets = assets;
-        this.assertDependencies();
-
-        // Clean up previous state's configurations.
-        // IMPLEMENT this in a way which does not break the state-stack.
-        // These two statements are not for clearing vars like MapCursor.position, that should
-        // be handled in .prev(). These are purely for enabling/disabling visual/interactive components.
-        this.controls.disableAll();
-        this.controlScripts.disableAll();
-
-        this.configureScene();  // Explicitly enable UI elements and interactibles.
-        Game.scene.ticker.add(this.update, this);   // TODO This prevents me from controlling the update step in the stack. Without controls, anyway.
+    open() {
+        this.configureScene();
     }
+
+    close() { }
 
     destroy() {
-        this.controls = null;
-        this.controlScripts = null;
-        Game.scene.ticker.remove(this.update, this);
+        //@ts-ignore
+        this.controller = null;
     }
 
-    nextState: nextState | null = null;
+    nextState: NextState | null = null;
+
+    // Move to 'real' implementations of TurnState
     advanceStates = {
         // Ex.
-        pickAttackTarget: {state: new PickAttackTargetState(), pre: () => {} },
-        finalizeState: {state: new FinalizeOrderState(), pre: () => {} }
+        pickAttackTarget: {state: PickAttackTargetState, pre: () => {} },
+        finalizeState: {state: FinalizeOrderState, pre: () => {} }
 
         // When picking an action, each action leads to a different scenario.
         // pickAttackTarget would let you pick an adjacent unit to brutalize.
@@ -61,17 +58,23 @@ export abstract class TurnState {
         // .next() runs a callback described by the given new state to do any pre-setup this state might need to.
         // Often, it probably won't need to.
         // I'm tired.
-    }
 
-    // IMPLEMENT Not sure where, but something needs to signal/return the new state we're advancing to.
-    // .next() is mostly a cleanup function. Probably useless, but maybe necessary.
-    next() {
-        if (this.nextState)
-            this.nextState.pre();
+        // pre(), speicifically, is probably not needed,
+        // but a way to send messages to new states will be.
+        // controller.assets is ~kind of~ that service, but I dunno.
+
+        // Basically, I need to decide if 'attack execution step' gets which
+        // attack is to be done from controller.assets or a special message from
+        // the previous state. Assets sounds easier, but I wonder.
     }
 }
 
-type nextState = {
-    state: TurnState,
+type TurnStateConstructor = {
+    new (cont: TurnStateController): TurnState;
+}
+
+// I'm saving this as an idea in case I run into a scenario that might need it.
+type NextState = {
+    state: TurnStateConstructor,
     pre: Function
 }
--- src/scripts/battle/turn-machine/TurnStateController.ts
+++ src/scripts/battle/turn-machine/TurnStateController.ts
@@ -1,17 +1,33 @@
 import { TurnState } from "./TurnState";
 import { Debug } from "../../DebugUtils";
+import { Game } from "../../..";
 
-const STACK_TRACE_LIMIT = 20;
 const STACK_SIZE_LIMIT = 100;   // Unenforced, but allocated.
 
+/** Operates the game's turn-based battle system. */
 export class TurnStateController {
 
     assets: BattleSceneAssets;
 
-    stack = new Array<TurnState>(STACK_SIZE_LIMIT);
-    stackTrace = new Array<string>(STACK_TRACE_LIMIT);
+    private stack = new Array<TurnState>(STACK_SIZE_LIMIT);
 
-    get currentStep(): TurnState {
+    constructor() {
+        // TODO Open the FIRST game state here.
+        Game.scene.ticker.add(this.update, this);
+    }
+
+    destroy() {
+        //@ts-ignore
+        this.assets = null;     // TODO Since this class likely constructs these, it should probably dismantle them, too.
+
+        // Clear inter-references to states and this controller.
+        this.stack.forEach( state => {
+            state.destroy();
+        })
+        this.stack = [];
+    }
+
+    private get currentStep(): TurnState {
         return this.stack[this.stack.length - 1];
     }
 
@@ -20,43 +36,61 @@ export class TurnStateController {
         this.currentStep.update();
     }
 
+    /** Advances the game's state to the given TurnState class. */
     next(step: TurnStateConstructor) {
         let newStep = new step(this);
 
-        this.currentStep.close();   // Finalize current step details
-        this.stack.push(newStep);   // Add new step to stack
-        this.log(this.currentStep.name);     // Log the new step in stack trace
-        if (this.currentStep.assert())  // Assert that new step was set up properly.
-            this.printStackTrace();
-        this.currentStep.open();    // Run new step's scene setup.
+        this.currentStep.close();
+        this.advanceToStep(newStep);
+        this.assertState();
+        this.currentStep.open();
     }
 
+    /** Reverts the game's state to the last stable one ('stable' meaning it does not skip on reversion.) */
     prev() {
+        this.currentStep.undo();
         this.currentStep.close();
-        this.stack.pop();
-        this.log(this.currentStep.name);
+        this.revertStep();
+
+        // TODO Predicklement: what if skipOnUndo = true does not allow us to reach a stable regression state?
+        // Like, in the configuration [true, true, →false] where we attempt to revert, we end up in the configuration
+        // [→true, true, false] and unable to revert any further. How do we then advance back up to the original configuration?
+        
+        // Something else, should all states be able to be regressed to?
 
         // If the step being switched to should not execute during regression, call the next previous step and abort this one.
         if (this.currentStep.skipOnUndo) {
-            this.currentStep.prev();
+            this.currentStep.undo();
         // Otherwise, confirm this step is still set up properly and let it set up the scene.
         } else {
-            this.currentStep.assert();
+            this.assertState();
             this.currentStep.open();
         }
     }
 
-    private log(msg: string) {
-        this.stackTrace.push(msg);
-        if (this.stackTrace.length > STACK_TRACE_LIMIT)
-            this.stackTrace.shift();    // Saves memory (I guess), but is it inefficient?
+    /** Advances the controller's current state to the given state object. */
+    private advanceToStep(step: TurnState) {
+        this.stack.push(step);
     }
 
+    /** Reverts the controller's current state to the previous state object in the stack. Future states are not preserved. */
+    private revertStep() {
+        this.stack.pop();
+    }
+
+    /** Asserts that the current state's execution conditions are met, and if not throws an error. */
+    private assertState() {
+        if (this.currentStep.assertDependencies() == false)
+            this.printStackTrace();
+    }
+
+    /** Prints the controller's state history as far back as it goes. */
     private printStackTrace() {
+        let c = 0;
         let str = "";
-        this.stackTrace.forEach( item => {
-            str = item + "\n" + str;
-        });
-        Debug.ping(str);
+        this.stack.forEach( state => {
+            str = `${c}: ${state.name}\n` + str;
+        })
+        Debug.assert(false, "Game state trajectory failed. Stack Trace:\n" + str);
     }
 }
