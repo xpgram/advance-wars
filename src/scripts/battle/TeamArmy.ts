344238a05018ef6c71c64c069dbefe3411973f81
Devin Biddy <xp.gram@gmail.com>
2021-04-10 13:54:32 -0700

    add TeamArmy and TurnModerator class details

--- src/scripts/battle/TeamArmy.ts
+++ src/scripts/battle/TeamArmy.ts
@@ -1,5 +1,13 @@
+import { UnitObject } from "./UnitObject";
+import { Slider } from "../Common/Slider";
 
+const MAX_UNITS = 50;
 
 export class TeamArmy {
-
+    color: 'red' | 'blue' | 'yellow' | 'black'; // TODO What is the enum for this?
+    officer: OfficerObject;     // Reference to CO class, like Unit.??? or Terrain.???
+    powerMeter: Slider;         // How much power meter is charged.
+    funds: number;              // Funds available for spending.
+    occupiedCityCount: number;  // The count of fund-giving cities owned; probs obtained from Map.
+    units: UnitObject[] = [];   // List of units under control.
 }
---
+++ src/scripts/battle/TurnModerator.ts
@@ -0,0 +1,7 @@
+
+
+export class TurnModerator {
+    // This keeps track of current turn-boy and next turn boy.
+    // Just a ticker that returns whoever the current TeamArmy is and can be,
+    // you know, ticked to the next one.
+}
