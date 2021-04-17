607b803702916f7174832f81b29ba4747960e203
Devin Biddy <xp.gram@gmail.com>
2021-04-17 11:29:53 -0700

    add player-team and turn-change infrastructure

--- src/scripts/battle/TurnModerator.ts
+++ src/scripts/battle/TurnModerator.ts
@@ -1,7 +1,29 @@
+import { TeamArmy } from "./TeamArmy";
+import { Slider } from "../Common/Slider";
 
-
+/** Keeps track of the current turn-player and the turn-taking order. */
 export class TurnModerator {
-    // This keeps track of current turn-boy and next turn boy.
-    // Just a ticker that returns whoever the current TeamArmy is and can be,
-    // you know, ticked to the next one.
+
+    readonly teams: TeamArmy[] = [];    // List of players
+    currentIdx: Slider;                 // Turn-player index
+
+    constructor() {
+        this.teams = [
+            new TeamArmy(),     // TODO This is temporary.
+            new TeamArmy(),     // Should probably get details or at least player count passed in.
+        ];                      // Also, what about pre-deploy?
+        this.currentIdx = new Slider({
+            max: this.teams.length,
+            granularity: 1,
+            looping: true,
+        });
+    }
+
+    get currentTeam() {
+        return this.teams[this.currentIdx.output];
+    }
+
+    changeToNext() {
+        this.currentIdx.increment();
+    }
 }
