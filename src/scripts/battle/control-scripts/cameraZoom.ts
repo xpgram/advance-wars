58a2f6fd816d7a919fca48b4c84a452cc066dff7
Devin Biddy <xp.gram@gmail.com>
2021-10-23 15:59:32 -0700

    name hardcoded number

--- src/scripts/battle/control-scripts/cameraZoom.ts
+++ src/scripts/battle/control-scripts/cameraZoom.ts
@@ -5,10 +5,16 @@ import { Camera } from "../../Camera";
 import { Game } from "../../..";
 
 export class CameraZoom extends ControlScript {
+
+    readonly zoomOutMaxAdditionalTiles = 10;
     
     private gamepad: VirtualGamepad;
     private camera: Camera;
-    private zoomSlider: Slider;
+
+    private zoomSlider = new Slider({
+        track: 'max',
+        granularity: 0.1
+    });
 
     defaultEnabled() { return true; }
 
@@ -17,11 +23,6 @@ export class CameraZoom extends ControlScript {
 
         this.gamepad = gp;
         this.camera = camera;
-
-        this.zoomSlider = new Slider({
-            track: 'max',
-            granularity: 0.1
-        });
     }
 
     protected enableScript(): void {
@@ -30,7 +31,7 @@ export class CameraZoom extends ControlScript {
 
     protected updateScript(): void {
         let w1 = Game.display.renderWidth;
-        let w2 = w1 + 8*Game.display.standardLength;
+        let w2 = w1 + this.zoomOutMaxAdditionalTiles*Game.display.standardLength;
         let widthsRatio = w1/w2;  // Ratio between the two desired pixel-width views of the map.
 
         if (this.gamepad.button.Y.pressed)
