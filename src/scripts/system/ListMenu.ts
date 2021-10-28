57cd1f702a5d37e596c8beb781e1729bcd2d604d
Dei Valko <xp.gram@gmail.com>
2021-10-28 16:15:22 -0700

    implement generic menu and menu-option classes

--- src/scripts/system/ListMenu.ts
+++ src/scripts/system/ListMenu.ts
@@ -1,207 +1,143 @@
+import { Game } from "../..";
+import { Slider } from "../Common/Slider";
+import { VirtualGamepad } from "../controls/VirtualGamepad";
+import { Pulsar } from "../timer/Pulsar";
+import { ListMenuOption } from "./ListMenuOption";
+
+type CursorSettings = {
+  firstFrameInterval: number,
+  frameInterval: number,
+}
 
 /** User-interactable UI element for seleting one of many options.
  * MenuWindow should be provided a list of MenuOptions with which to populate
  * itself and will return the value of the selected-over option upon request.
  */
-export class ListMenu {
+export class ListMenu<X, Y> {
 
-    static readonly cursorSettings = {
-        moveFrames: 3,          // How many frames to change selector states
-        pulseInterval: 5,       // How long to wait between cursor changes (frames).
-        pulseIntervalFirst: 20, // How long to wait before the first cursor auto-change (frames).
+  private readonly gamepad: VirtualGamepad;
+
+  private _listItems!: ListMenuOption<X, Y>[];
+  private _displayedListItems!: ListMenuOption<X, Y>[];
+  private _inputEnabled = true;
+
+  /** Represents the currently selected option. */
+  private cursor!: Slider;
+
+  /** The timer which triggers repeatable movements. */
+  private movementPulsar: Pulsar;
+
+  constructor(gp: VirtualGamepad, options?: {
+    listItems?: ListMenuOption<X, Y>[],
+    cursorSettings?: CursorSettings,
+  }) {
+    // Setup option defaults
+    const _options = {
+      listItems: [],
+      cursorSettings: {
+        firstFrameInterval: 20,
+        frameInterval: 6,
+      },
+      ...options,
     }
 
-    private readonly gamepad: VirtualGamepad;
+    // Configure
+    this.gamepad = gp;
+    this.setListItems(_options.listItems);
 
-    readonly transform = new LowResTransform();
+    // Add updater to global ticker.
+    Game.scene.ticker.add(this.update, this);
 
-    private readonly graphics = new PIXI.Container();
-    private readonly background = new PIXI.Container();
-    private readonly optionsText = new PIXI.Container();
-    private readonly gCursor = new PIXI.Container();
+    // Initiate timers
+    this.movementPulsar = new Pulsar({
+      firstInterval: _options.cursorSettings.firstFrameInterval,
+      interval: _options.cursorSettings.frameInterval,
+    },
+      this.triggerCursorMovement,
+      this
+    );
+  }
 
-    // TODO Graphics structure
-    // How do I determine/enforce an option is disabled/greyed-out?
-    // I think optionsGraphics = Container() needs to be optionsGraphics = container[]
+  /** Unlinks circular connections. */
+  destroy() {
+    Game.scene.ticker.remove(this.update, this);
+  }
 
-    // global
-    //  \- background
-    //      \- option []
-    //          \- box graphic
-    //              \- text
+  private update() {
+    this.updateCursor();
+  }
 
-    private _options: MenuOption<number>[] = [];
-    private _enabled = true;
+  /** Updates cursor state in accordance with gamepad input. */
+  private updateCursor() {
+    if (!this.inputEnabled)
+      return;
 
-    /** Represents the currently selected option. */
-    private cursor = new Slider();
-
-    /** The timer which triggers animation pulses. */
-    private animPulsar: Pulsar;
-
-    /** Guides the option selector's position on-screen as it animates from one selectable to another. */
-    private stateChangeAnimSlider = new Slider({
-        granularity: 1 / MenuWindow.cursorSettings.moveFrames,
-    })
-
-    /** The timer which triggers repeatable movements. */
-    private movementPulsar: Pulsar;
-
-    constructor(gp: VirtualGamepad, ui: PIXI.Container) {
-        // Connect graphical peripheries
-        this.transform.object = this.graphics;
-        ui.addChild(this.graphics);
-
-        // Connect controller.
-        this.gamepad = gp;
-
-        // Invoke options configurer
-        this.options = [];
-
-        // Add updater to global ticker.
-        Game.scene.ticker.add( this.update, this );
-
-        // Initiate timers.
-        this.movementPulsar = new Pulsar(
-            MenuWindow.cursorSettings.pulseIntervalFirst,
-            this.triggerCursorMovement,
-            this
-        );
-
-        // TODO Remove; test
-        const cursor = newSelectorGraphic(Point.Origin, 0);
-        this.gCursor.addChild(cursor);
-
-        this.graphics.addChild(this.background);
-        this.graphics.addChild(this.gCursor);
-        this.graphics.addChild(this.optionsText);
-        this.transform.z = 110;
+    // Input polling.
+    const { dpadUp, dpadDown } = this.gamepad.button;
+    if (dpadUp.pressed || dpadDown.pressed) {
+      this.triggerCursorMovement();
+      this.movementPulsar.start();
     }
-
-    destroy() {
-        this.transform.destroy();
-        this.graphics.destroy({children: true});
-        
-        Game.scene.ticker.remove( this.update, this );
+    if (this.gamepad.axis.dpad.returned) {
+      this.movementPulsar.stop();
     }
+  }
 
-    private update() {
-        this.updateCursor();
-        this.updateCursorPosition();
-    }
+  /** Triggers a cursor change according the held player inputs. */
+  private triggerCursorMovement() {
+    const dir = this.gamepad.axis.dpad.point.y;
+    this.cursor.increment(dir);
+  }
 
-    /** Updates cursor state in accordance with gamepad input. */
-    private updateCursor() {
-        if (!this.enabled)
-            return;
-        
-        // Input polling.
-        if (this.gamepad.button.dpadUp.pressed) {
-            this.cursor.decrement();
-            this.movementPulsar.start();    // TODO Build in a first pulse timer? Would simplify.
+  /** Enables the player interactivity listener. */
+  enableInput() {
+    this._inputEnabled = true;
+  }
 
-            this.background.removeChildren();
-            this.background.addChild( newMenuGraphic(this._options, this.cursor.output) );
-        }
-        else if (this.gamepad.button.dpadDown.pressed) {
-            this.cursor.increment();
-            this.movementPulsar.start();
+  /** Disables the player interactivity listener. */
+  disableInput() {
+    this._inputEnabled = false;
+    this.movementPulsar.stop();
+  }
 
-            this.background.removeChildren();
-            this.background.addChild( newMenuGraphic(this._options, this.cursor.output) );
-        }
+  /** Whether this menu is interactable. */
+  get inputEnabled() {
+    return this._inputEnabled;
+  }
 
-        if (this.gamepad.axis.dpad.neutral) {
-            this.movementPulsar.stop();
-            this.movementPulsar.interval = MenuWindow.cursorSettings.pulseIntervalFirst;
-        }
-    }
+  /** The list of selectables as key/value pairs. */
+  get listItems() {
+    return this._displayedListItems.slice();
+  }
 
-    /** Updates selector screen/world position depending on animation states. */
-    private updateCursorPosition() {
-        const vert = OPTION_PROPS.containerBox().height * this.cursor.output + MENU_PROPS.padding.top;
-        this.gCursor.position.set(MENU_PROPS.padding.left, vert);
-    }
+  /** Retriggers configuration of each list item to update which are included
+   * and which are disabled. Will possibly update the options list. */
+  retriggerListItems() {
+    this._listItems.forEach( item => item.retrigger() );
+    this._displayedListItems = this._listItems.filter( item => item.included );
+    this.cursor = new Slider({
+      max: this._displayedListItems.length,
+      track: 'min',
+      granularity: 1,
+      looping: true,
+      // mode: 'loop',    // TODO Slider refactor was unsuccessful.
+    });
+  }
 
-    /** Triggers a cursor change according the held player inputs. */
-    private triggerCursorMovement() {
-        const dir = this.gamepad.axis.dpad.point.y;
-        this.cursor.increment(dir);
-        this.movementPulsar.interval = MenuWindow.cursorSettings.pulseInterval;
+  /** Sets a new list of menu options. It is recommended to use triggerInclude on each
+   * list-option instead to automatically add or remove them from the menu. */
+  setListItems(li: ListMenuOption<X, Y>[]) {
+    this._listItems = li;
+    this.retriggerListItems();
+  }
 
-        this.background.removeChildren();
-        this.background.addChild( newMenuGraphic(this._options, this.cursor.output) );
-    }
+  /** Returns the ListMenuOption currently being selected over. */
+  get selectedOption() {
+    return this._listItems[this.cursor.output];
+  }
 
-    /** Reveals the menu's graphics and enables player controls. */
-    show() {
-        this.enable();
-        this.graphics.visible = true;
-    }
-
-    /** Hides the menu's graphics and disables player controls. */
-    hide() {
-        this.disable();
-        this.graphics.visible = false;
-    }
-
-    /** Whether this menu is invisible and uninteractable. */
-    get hidden() {
-        return (!this.graphics.visible);
-    }
-
-    /** Enables the player interactivity listener. */
-    enable() {
-        this._enabled = true;
-    }
-
-    /** Disables the player interactivity listener. */
-    disable() {
-        this._enabled = false;
-        this.movementPulsar.stop();
-        this.movementPulsar.interval = MenuWindow.cursorSettings.pulseIntervalFirst;
-    }
-
-    /** Whether this menu is interactable. */
-    get enabled() {
-        return this._enabled;
-    }
-
-    /** The list of selectables as text/value pairs. */
-    get options() {
-        return this._options.slice();
-    }
-    set options(li: MenuOption<number>[]) {
-        this._options = li;
-        // reconfigure box height/width
-        
-        this.cursor = new Slider({
-            max: this._options.length,
-            track: 'min',
-            granularity: 1,
-            looping: true,
-            // mode: 'loop',
-        });
-
-        OPTION_PROPS.width = this.getNewContentWidth();
-        MENU_PROPS.children = Array(li.length).fill(OPTION_PROPS);
-
-        this.background.removeChildren();
-        this.background.addChild( newMenuGraphic(this._options, this.cursor.output) );
-    }
-
-    /** Returns the value currently being selected over. */
-    get selectedValue() {
-        return this._options[this.cursor.output].value;
-    }
-
-    /** Returns the pixil-width of the longest option name in this menu's list of options. */
-    private getNewContentWidth() {
-        if (this._options.length === 0)
-            return 0;
-
-        const tmp = new PIXI.BitmapText('', fonts.menu);
-        const sizes = this._options.map( o => {tmp.text = o.text; return tmp.textWidth;} );
-        return sizes.reduce( (max, cur) => (max > cur) ? max : cur );
-    }
+  /** Returns the value currently being selected over. */
+  get selectedValue() {
+    return this.selectedOption.value;
+  }
 }
--- src/scripts/system/ListMenuOption.ts
+++ src/scripts/system/ListMenuOption.ts
@@ -8,11 +8,19 @@ export class ListMenuOption<X, Y> {
 
   /** A function which should return true if this option is to be included in its menu.
    * Default function always returns true. */
-  included: () => boolean = () => { return true; }
+  private triggerInclude: () => boolean = () => true;
 
   /** A function which should return true if this option should not be selectable.
    * Default function always returns false. */
-  disabled: () => boolean = () => { return false; }
+  private triggerDisable: () => boolean = () => false;
+
+  /** Whether this option should be included in the list. */
+  get included() { return this._included; }
+  private _included: boolean = true;
+
+  /** Whether this option should be selectable. */
+  get disabled() { return this._disabled; }
+  private _disabled: boolean = false;
 
   constructor(key: X, value: Y, options?: {
     triggerInclude?: () => boolean,
@@ -20,7 +28,13 @@ export class ListMenuOption<X, Y> {
   }) {
     this.key = key;
     this.value = value;
-    this.included = options?.triggerInclude || this.included;
-    this.disabled = options?.triggerDisable || this.disabled;
+    this.triggerInclude = options?.triggerInclude || this.triggerInclude;
+    this.triggerDisable = options?.triggerDisable || this.triggerDisable;
+  }
+
+  /** Re-evaluates list-include and list-disable settings for this option. */
+  retrigger() {
+    this._included = this.triggerInclude();
+    this._disabled = this.triggerDisable();
   }
 }
