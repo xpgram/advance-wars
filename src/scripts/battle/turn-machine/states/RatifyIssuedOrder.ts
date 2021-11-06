b5eecab1c873aca7ecda4764e97b52caa1aa33eb
Devin Biddy <xp.gram@gmail.com>
2021-11-05 18:48:01 -0700

    unbreak unit spawning

--- src/scripts/battle/turn-machine/states/RatifyIssuedOrder.ts
+++ src/scripts/battle/turn-machine/states/RatifyIssuedOrder.ts
@@ -32,12 +32,13 @@ export class RatifyIssuedOrder extends TurnState {
   protected configureScene(): void {
     const get = this.assertData.bind(this);
     const { map, instruction } = this.assets;
-    const { actor } = this.data;
 
     const nonActorInstructions = [Instruction.SpawnUnit, Instruction.SpawnLoadUnit];
 
     const action = get(instruction.action, `serial for action to be taken`);
     const location = get(instruction.place, 'location of actor');
+
+    const tileEmpty = (!map.squareAt(location).unit);
 
     // Revert settings set for TrackCar.
     map.squareAt(location).hideUnit = false;
@@ -47,35 +48,23 @@ export class RatifyIssuedOrder extends TurnState {
     command.ratify();
 
     // Drop units // TODO bad implementation; doesn't make use of Drop.ratify()
-    this.data.drop
-      .sort( (a,b) => b.which - a.which )
-      .forEach( d => {
-        const { which, where } = d;
-        const unit = actor.unloadUnit(which);
-        map.placeUnit(unit, where);
-        unit.spent = true;
-      });
+    if (!tileEmpty) {
+      const { actor } = this.data;
+      this.data.drop
+        .sort( (a,b) => b.which - a.which )
+        .forEach( d => {
+          const { which, where } = d;
+          const unit = actor.unloadUnit(which);
+          map.placeUnit(unit, where);
+          unit.spent = true;
+        });
+    }
 
     // Update player controls.
     if (instruction.path) {
       const path = get(instruction.path, `actor's movement path`);
       const destination = SumCardinalVectorsToVector(path).add(location);
       this.assets.mapCursor.teleport(destination);
-    }
-
-    // logic for non-actor actions
-    // TODO FactoryMenu does not use Command.ts yet, so this is still necessary for unit spawning.
-    if (nonActorInstructions.includes(action)) {
-      // Spawn Unit
-      if (instruction.action === Instruction.SpawnUnit) {
-        const which = get(instruction.which, `actor's action variant for enumerable ${action}`);
-        const unit = this.assets.players.current.spawnUnit({
-          location,
-          serial: which,
-          spent: true,
-        });
-        this.assets.players.current.expendFunds(unit.cost);
-      }
     }
 
     // Advance to next state.
