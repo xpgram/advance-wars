feba7fd408f0aae31aea6f3fba56ab985d9771e9
Dei Valko <xp.gram@gmail.com>
2021-11-11 15:23:38 -0800

    refactor for more deliberate drawing

--- src/scripts/system/gui-menu-components/CommandMenuGUI.ts
+++ src/scripts/system/gui-menu-components/CommandMenuGUI.ts
@@ -1,11 +1,12 @@
 import { Game } from "../../..";
 import { fonts } from "../../battle/ui-windows/DisplayInfo";
 import { BoxContainerProperties } from "../../Common/BoxContainerProperties";
-import { Point } from "../../Common/Point";
 import { Color } from "../../CommonUtils";
 import { ListMenu } from "./ListMenu";
 import { ListMenuOption } from "./ListMenuOption";
 import { MenuCursor } from "./MenuCursor";
+
+// This is 'done' but I'm scared.
 
 
 const { HSV } = Color;
@@ -24,6 +25,13 @@ export class CommandMenuGUI<X, Y> {
   /** The cursor graphic which visually selects over the menu. */
   private cursorGraphic: MenuCursor;
 
+  /** A list of all list-item textures; textures for each state. */
+  private stateTextures!: {
+    enabled: PIXI.Texture,
+    disabled: PIXI.Texture,
+    selected: PIXI.Texture,
+  };
+
   /** Reference to this menu's pseudo-css properties. */
   readonly listItemProps = new BoxContainerProperties({
     minWidth: 40,
@@ -34,26 +42,23 @@ export class CommandMenuGUI<X, Y> {
   });
 
   readonly palette = {
-    selector:     HSV(166, 100, 80),
-    button: {
-      unselected: {
-        background: HSV(200, 30, 30),
-        primary:    HSV(215, 25, 35),
-        light:      HSV(220, 15,100),
-        dark:       HSV(190, 10,  0),
-      },
-      selected: {
-        background: HSV(200, 30, 30),
-        primary:    HSV(170, 65, 40),
-        light:      HSV(185, 35,  0),
-        dark:       HSV(170, 35,100),
-      },
-      disabled: {
-        background: HSV(200, 0, 20),
-        primary:    HSV(215, 0, 30),
-        light:      HSV(190, 0,  0),
-        dark:       HSV(220, 0, 80),
-      },
+    unselected: {
+      background: HSV(200, 30, 30),
+      primary:    HSV(215, 25, 35),
+      light:      HSV(220, 15,100),
+      dark:       HSV(190, 10,  0),
+    },
+    selected: {
+      background: HSV(200, 30, 30),
+      primary:    HSV(170, 65, 40),
+      light:      HSV(185, 35,  0),
+      dark:       HSV(170, 35,100),
+    },
+    disabled: {
+      background: HSV(200, 0, 20),
+      primary:    HSV(215, 0, 30),
+      light:      HSV(190, 0,  0),
+      dark:       HSV(220, 0, 80),
     },
   }
 
@@ -69,7 +74,7 @@ export class CommandMenuGUI<X, Y> {
 
     this.menu = menu;
     this.menu.on('move-cursor', () => {
-      this.buildGraphics();
+      this.buildListItems();
       const element = this.listItemProps.containerBox();
       this.cursorGraphic.rect = new PIXI.Rectangle(
         element.x,
@@ -77,22 +82,14 @@ export class CommandMenuGUI<X, Y> {
         element.width,
         element.height
       );
-
-      // TODO Build elements for each palette, save as AnimatedSprite.
-      // TODO Update list-item frames: all→unselected/disabled, one→selected/disabled
-      // TODO Remove build() on every frame.
     });
 
     container.addChild(this.gui);
     this.gui.addChild(this.menuGui);
     this.cursorGraphic = new MenuCursor(this.gui);
 
-    this.buildGraphics();
-
-    // TODO Remove; build should happen once and saved.
-    // Did I only do this because of the cursor?
-    // TODO Oh, it was also for selected vs. unselected.
-    Game.scene.ticker.add(() => { this.buildGraphics() });
+    this.buildTextures();
+    this.buildListItems();
   }
 
   /** Unlinks this object's circular references and removes it from higher scope structures. */
@@ -100,6 +97,7 @@ export class CommandMenuGUI<X, Y> {
     this.gui.destroy({children: true});
     this.menu.destroy();
     this.cursorGraphic.destroy();
+    Object.values(this.stateTextures).forEach( t => t.destroy() );
   }
 
   /** Reveals this menu's graphics and enables player input. */
@@ -123,7 +121,7 @@ export class CommandMenuGUI<X, Y> {
   /** Sets a new list of menu options, and rebuilds the GUI's graphics. */
   setListItems(li: ListMenuOption<X,Y>[]) {
     this.menu.setListItems(li);
-    this.buildGraphics();
+    this.buildListItems();
     this.cursorGraphic.skipMotion();
   }
 
@@ -142,75 +140,98 @@ export class CommandMenuGUI<X, Y> {
     return Math.max(...sizes);
   }
 
-  /** Builds a graphical representation of this  */
-  buildGraphics() {
+  /** Draws the button textures for each possible button state. */
+  private buildTextures() {
+    const { palette, stateTextures, listItemProps: props } = this;
+
+    type buttonPalette = typeof palette.unselected; // TODO ? Why not officially declare it?
     this.listItemProps.width = this.getContentWidth();
-
-    const menu = new PIXI.Graphics();
-    const props = this.listItemProps;
-
-    // Reset containers
-    this.menuGui.removeChildren();
-    this.menuGui.addChild(menu);
-
-    /* List Items */
-
-    const worldPosition = new Point(menu);
-
-    this.menu.listItems.forEach( (item, idx) => {
-      const palette = (item.disabled)
-        ? this.palette.button.disabled
-        : (idx === this.menu.selectedIndex)
-        ? this.palette.button.selected
-        : this.palette.button.unselected;
-
+    
+    function build(palette: buttonPalette): PIXI.Texture {
       const g = new PIXI.Graphics();
-      const content = props.contentBox();
+
       const fill = props.borderInnerBox();
       const border = props.borderOuterBox();
       const element = props.containerBox();
 
-      // Position
-      g.position.set(
-        worldPosition.x,
-        worldPosition.y + props.elementHeight*idx
-      );
-
       // Background
       g.beginFill(palette.background);
       g.drawRect(element.x, element.y, element.width, element.height);
-      g.endFill();
 
-      // Button Fill
+      // Button fill
       g.beginFill(palette.primary);
       g.drawRect(fill.x, fill.y, fill.width, fill.height);
-      g.endFill();
 
       // Shadowed Border
       g.beginFill(palette.dark, .50);
       g.drawRect(border.x, border.y + border.height, border.width, -props.border.bottom);
       g.drawRect(border.x + border.width, border.y, -props.border.right, border.height);
-      g.endFill();
 
-      // Lit Border
+      // Lit border
       g.beginFill(palette.light, .25);
       g.drawRect(border.x, border.y, props.border.left, border.height);
-      g.beginFill(palette.light, .50);
       g.drawRect(border.x, border.y, border.width, props.border.top);
+
       g.endFill();
 
-      // Text   // TODO Make this overridable
-      const { key } = this.menu.listItems[idx];
-      const gText = new PIXI.BitmapText(key, fonts.menu);
-      if (item.disabled)
-        gText.tint = 0x888888;
-      gText.position.set(content.x + content.width*.5, content.y + 1);
-      gText.anchor.set(.5, 0);
+      // Save texture
+      return Game.app.renderer.generateTexture(g);
+    }
+    
+    Object.values(stateTextures).forEach( t => t.destroy() );
+    stateTextures.enabled = build(palette.unselected);
+    stateTextures.disabled = build(palette.disabled);
+    stateTextures.selected = build(palette.selected);
+  }
+
+  /** Builds the list of list-item graphics objects. */
+  buildListItems() {
+    const { menu, stateTextures, listItemProps: props } = this;
+
+    const content = props.contentBox();
+    const element = props.containerBox();
+
+    // Reset menu gui
+    this.menuGui.children.forEach( g => g.destroy() );
+    this.menuGui.removeChildren();
+
+    // Build a sprite for each element
+    menu.listItems.forEach( (item, idx) => {
+      const tex = (item.disabled)
+        ? stateTextures.disabled
+        : (menu.selectedOption === item)
+        ? stateTextures.selected
+        : stateTextures.enabled;
+
+      const spr = new PIXI.Sprite(tex);
+      spr.position.y = element.height * idx;
+
+      // Build text
+      const { key } = item;
+      const title = new PIXI.BitmapText(key, fonts.menu);
+      title.position.set(content.x + content.width*.5, content.y + 1);
+      title.anchor.set(.5, 0);
 
       // Combine
-      g.addChild(gText);
-      menu.addChild(g);
+      spr.addChild(title);
+      this.menuGui.addChild(spr);
     });
-
   }
+
+  /** Updates each list item with relevant state textures. */
+  updateFrames() {
+    const textures = this.stateTextures;
+    const objects = this.menuGui.children;
+
+    this.menu.listItems.forEach( (item, idx) => {
+      const object = objects[idx];
+      object.texture = (item.disabled)
+        ? textures.disabled
+        : (this.menu.selectedOption === item)
+        ? textures.selected
+        : textures.enabled;
+      object.children[0].tint = (item.disabled) ? 0x888888 : 0xFFFFFF;
+    });
+  }
+
 }
