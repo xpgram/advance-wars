728c8e98b0f5b500db91ffe585b7da7af914fd4f
Dei Valko <xp.gram@gmail.com>
2021-12-03 14:22:53 -0800

    add unit stat table (partial)

--- src/scripts/battle/CommandingOfficerObject.ts
+++ src/scripts/battle/CommandingOfficerObject.ts
@@ -1,4 +1,29 @@
 import { Game } from "../..";
+import { StringDictionary } from "../CommonTypes";
+
+type UnitStats = {
+  attack: number,
+  defense: number,
+  range: number,
+  move: number,
+  vision: number,
+}
+
+type UnitStatsPartial = {
+  attack?: number,
+  defense?: number,
+  range?: number,
+  move?: number,
+  vision?: number,
+}
+
+const DEFAULT_STATS: UnitStats = {
+  attack: 0,
+  defense: 0,
+  range: 0,
+  move: 0,
+  vision: 0,
+}
 
 export interface CommandingOfficerType {
   new (): CommandingOfficerObject;
@@ -14,10 +39,38 @@ export abstract class CommandingOfficerObject {
   abstract readonly allegiance: string;
   abstract readonly nationality: 'rubinelle' | 'lazurian';
 
-  readonly illustration!: PIXI.Sprite;
-  readonly eyeshot!: PIXI.Sprite;
-  readonly insignia!: PIXI.Sprite;
+  get illustration() { return this._illustration; }
+  private _illustration!: PIXI.Sprite;
 
+  get eyeshot() { return this._eyeshot; }
+  private _eyeshot!: PIXI.Sprite;
+
+  get insignia() { return this._insignia; }
+  private _insignia!: PIXI.Sprite;
+
+  readonly unitStatTable!: StringDictionary<UnitStats>;
+
+  /** Returns an entry from the unit stats table.
+   * All element names are acceptable; any non-entries will assume default values. */
+  getUnitStats(name: string): UnitStats {
+    const stats = this.unitStatTable[name];
+    return (stats)
+      ? stats
+      : DEFAULT_STATS;
+  }
+
+  /** Sets only the described fields for all given element names on the unit stats table. */
+  protected setUnitStats(stats: UnitStatsPartial, ...names: string[]) {
+    names.forEach( name => {
+      const oldStats = this.unitStatTable[name] || {};
+      stats = {...DEFAULT_STATS, ...oldStats, ...stats};
+      this.unitStatTable[name] = stats as UnitStats;
+    })
+  }
+
+  /** Initializes object for use.
+   * If overriding, must call super.init() as first line; consider this like
+   * you would a constructor. */
   init() {
     const sheet = Game.scene.resources['CoSpritesheet'].spritesheet as PIXI.Spritesheet;
     const uiSheet = Game.scene.resources['UISpritesheet'].spritesheet as PIXI.Spritesheet;
@@ -26,9 +79,9 @@ export abstract class CommandingOfficerObject {
     const allegiance = this.allegiance.toLowerCase().replace(' ', '-');
     const color = 0;
 
-    this.illustration = new PIXI.Sprite(sheet.textures[`${name}-p${color}-full.png`]);
-    this.eyeshot = new PIXI.Sprite(sheet.textures[`${name}-p${color}-eyes.png`]);
-    this.insignia = new PIXI.Sprite(uiSheet.textures[`insignia-${allegiance}.png`]);
+    this._illustration = new PIXI.Sprite(sheet.textures[`${name}-p${color}-full.png`]);
+    this._eyeshot = new PIXI.Sprite(sheet.textures[`${name}-p${color}-eyes.png`]);
+    this._insignia = new PIXI.Sprite(uiSheet.textures[`insignia-${allegiance}.png`]);
 
     return this;
   }
