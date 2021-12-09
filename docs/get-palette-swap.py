937310b8d1c243e99b8fe643ddc9c7cbbdb0b51a
Dei Valko <xp.gram@gmail.com>
2021-12-08 17:56:22 -0800

    finish palette swap script

--- docs/get-palette-swap.py
+++ docs/get-palette-swap.py
@@ -10,10 +10,10 @@ def path(filename):
 file1 = argv[1]
 file2 = argv[2]
 
-img_source = Image.open(path(file1), 'r')
+img_source = Image.open(path(file1), 'r').convert("RGBA")
 pixels_source = list(img_source.getdata())
 
-img_match = Image.optn(path(file2), 'r')
+img_match = Image.open(path(file2), 'r').convert("RGBA")
 pixels_match = list(img_match.getdata())
 
 if (len(pixels_source) != len(pixels_match)):
@@ -21,8 +21,8 @@ if (len(pixels_source) != len(pixels_match)):
 
 palette = {}
 
-def rgb_to_hexStr(rgb):
-  return '0x' + ('%02x%02x%02x' % rgb).upper()
+def rgba_to_hexStr(rgba):
+  return '0x' + ('%02x%02x%02x%02x' % rgba).upper()
 
 for i in range(0,len(pixels_source)):
   def colorMatch(a, b):
@@ -34,7 +34,7 @@ for i in range(0,len(pixels_source)):
   match = pixels_match[i]
 
   if not colorMatch(source, match):
-    palette[rgb_to_hexStr(source)] = rgb_to_hexStr(match)
+    palette[rgba_to_hexStr(source)] = rgba_to_hexStr(match)
 
 for key in palette:
   print(key, ' → ', palette[key])
