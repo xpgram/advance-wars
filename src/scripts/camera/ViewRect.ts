ec3c1fad2550a0de5313782c9cea2abb608ea7a4
Devin Biddy <xp.gram@gmail.com>
2021-12-16 09:15:03 -0800

    .?

--- src/scripts/camera/ViewRect.ts
+++ src/scripts/camera/ViewRect.ts
@@ -1,14 +1,15 @@
+import { Rectangle } from "pixi.js";
 
 export class ViewRect {
 
-  // I don't think ViewRect, a framing tool for camera, needs to know all this.
-  // But it can return a PIXI.Rect or my own; I probs gonna write my own 'cause I like to.
-  // I love math, yo.
+  // Currently, I think, all camera transform stuff gon' be extracted here.
+  // camera.frame.world({postZoom: true})
+  // camera.frame.subject()
+  // camera.frame.center.x = n
 
-  // basic: x, y, width, height, left, right, top, bottom
-  // camera: ?? (I forgot to be thinkin' 'bout it)
-  // .rect => getter returns Rectangle object
-  // .rect => setter conforms camera properties such that this ViewRect is equivalent to the given one
+
+  real: Rectangle;    // Current transformation
+  final: Rectangle;   // End-state after all planned transformations (zoom)
 
   constructor() {
     
