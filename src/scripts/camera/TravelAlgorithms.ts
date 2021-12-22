bc5186bece2345164131320312521085acc246bb
Dei Valko <xp.gram@gmail.com>
2021-12-22 14:46:44 -0800

    smooth zoom transitions

--- src/scripts/camera/TravelAlgorithms.ts
+++ src/scripts/camera/TravelAlgorithms.ts
@@ -12,6 +12,7 @@ export interface TravelAlgorithm {
 
 export class LinearApproach implements TravelAlgorithm {
   update(actual: ViewRect, target: ViewRect, focal: Point): ViewRect {
+    const { abs } = Math;
     const tileSize = Game.display.standardLength;
     const maxDist = Math.floor(tileSize*.5);
     const maxZoomDiff = .025;
@@ -20,22 +21,30 @@ export class LinearApproach implements TravelAlgorithm {
     actual.border = target.border;
 
     // Zoom
+    const zDiff = target.zoom - actual.zoom;
     const zVector = Common.clamp(
-      target.zoom - actual.zoom, -maxZoomDiff, maxZoomDiff);
-    if (zVector !== 0) {
+      zDiff, -maxZoomDiff, maxZoomDiff);
+    if (zVector !== 0)
       actual.zoomToPoint(actual.zoom + zVector, focal);
-      return actual;
-    }
-
-    // TODO I could clean up the minor movements needed after zoom if I knew
-    // what the final zoom position of actual would look like.
-    // Probably zoomToPoint(target.zoom, focal) would do this for me.
 
     // Move
+    const finalzoom = actual.clone();
+    finalzoom.zoomToPoint(target.zoom, focal);
+
+    // Func: when zooming, travel proportional to zoom completion, else cap speed as normal.
+    let zoomRatio = abs(zVector) / abs(zDiff);
+    const axisFunction = (x: number) => (target.zoom === actual.zoom)
+      ? Common.clamp(x, -maxDist, maxDist)
+      : x*zoomRatio;
+
+    // Limit travel vector
     const tVector = new Point(target.worldRect())
-      .subtract(new Point(actual.worldRect()))
-      .apply(x => Common.clamp(x, -maxDist, maxDist));
+      .subtract(new Point(finalzoom.worldRect()))
+      .apply(axisFunction);
+
+    // Apply travel vector
     actual.position = actual.position.add(tVector);
+
     return actual;
   }
 }
