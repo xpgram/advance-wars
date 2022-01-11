2ccaf257ea68304a8096a1acb1171c3b27f8cc3f
Dei Valko <xp.gram@gmail.com>
2022-01-11 14:35:57 -0800

    update todo

--- src/scripts/battle/ui-windows/HudIndicators.ts
+++ src/scripts/battle/ui-windows/HudIndicators.ts
@@ -6,22 +6,16 @@ export class HudIndicators extends SlidingWindow {
 
   dayCounter = new DayCounter();
 
+  // TODO I want to move this to a separate slider which doesn't rotate around
+  // the view but is 'pushed' from the left by the one that does.
+
   constructor(options: SlidingWindowOptions) {
     super(options);
     this.dayCounter.container.x = 4;
 
-    const test1 = {container: new PIXI.Sprite(this.sheet.textures[`icon-orderable-bases.png`])};
-    test1.container.position.set(24, 4);
-    test1.container.scale.set(.5);
-    const test2 = {container: new PIXI.Sprite(this.sheet.textures[`icon-orderable-units.png`])};
-    test2.container.position.set(40, 4);
-    test2.container.scale.set(.5);
-
     this.displayContainer.addChild(
       ...[
         this.dayCounter,
-        test1,
-        test2,
       ].map( e => e.container )
     );
   }
