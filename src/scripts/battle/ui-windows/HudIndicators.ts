a09a775d8e7a9a68c6f553b66e787df586011a0b
Dei Valko <xp.gram@gmail.com>
2022-01-10 18:56:38 -0800

    setup visual test

--- src/scripts/battle/ui-windows/HudIndicators.ts
+++ src/scripts/battle/ui-windows/HudIndicators.ts
@@ -10,9 +10,18 @@ export class HudIndicators extends SlidingWindow {
     super(options);
     this.dayCounter.container.x = 4;
 
+    const test1 = {container: new PIXI.Sprite(this.sheet.textures[`icon-orderable-bases.png`])};
+    test1.container.position.set(24, 4);
+    test1.container.scale.set(.5);
+    const test2 = {container: new PIXI.Sprite(this.sheet.textures[`icon-orderable-units.png`])};
+    test2.container.position.set(40, 4);
+    test2.container.scale.set(.5);
+
     this.displayContainer.addChild(
       ...[
         this.dayCounter,
+        test1,
+        test2,
       ].map( e => e.container )
     );
   }
