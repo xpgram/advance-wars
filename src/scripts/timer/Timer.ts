df58afaff4ab413edce0ce03b42058735f4d22b2
Dei Valko <xp.gram@gmail.com>
2022-01-26 14:27:17 -0800

    first draft; untested

--- src/scripts/timer/Timer.ts
+++ src/scripts/timer/Timer.ts
@@ -1,175 +1,288 @@
 import { Game } from "../..";
 import { Slider } from "../Common/Slider";
 
-const MILLIS = 1000;
 
-/** A single-pulse timer which calls a callback (if one is given) one time after
- * the timer has fully elapsed. An expired timer can be restarted by simply calling start(). */
+const TO_MILLIS = 1000;
+const TO_SECONDS = 0.001;
+
+interface TimerEvent {
+  time: number;
+  event: () => void;
+  context?: object;
+}
+
+/** Timer
+ * Something something inspired by Lua.
+ * 
+ * // TODO Multiple tweens/everys? I mean, why not, right?
+ */
 export class Timer {
 
-  private _started = false;
+  /** NULL function which fills in missing event functions on new Timer() calls. */
+  static readonly NULL_EVENT = () => {};
 
-  private elapsedTime: number = 0;
-  private timerLength: number = 0;
-  private callback: (() => void) | undefined;
+  /** NULL object which allows schedule retrieval from a TimerEvents list to be safe. */
+  static readonly NULL_SCHEDULE: TimerEvent = {
+    time: 0,
+    event: Timer.NULL_EVENT,
+  }
 
-  constructor(seconds: number, cb?: () => void) {
-    this.timerLength = seconds * MILLIS;
-    this.callback = cb;
+  /** Shortcut to new Timer().at(); returns a Timer object. */
+  static at(time: number, event: () => void, context?: object) {
+    return new Timer().at(time, event, context);
+  }
+
+  /** Shortcut to new Timer().every(); returns a Timer object. */
+  static every(time: number, event: () => void, context?: object) {
+    return new Timer().every(time, event, context);
+  }
+
+  /** Shortcut to new Timer().tween(); returns a Timer object. */
+  static tween(time: number, event: (n: number) => void, context?: object, shape?: (n: number) => number) {
+    return new Timer(time).tween(event, context, shape);
+  }
+
+  /** Whether this object destroys itself after calling the last event. */
+  private selfDestruct = true;
+
+  /** True if the timer's clock should be ticking. */
+  private started = false;
+
+  /** The current elapsed time in milliseconds. */
+  private elapsedMillis: number = 0;
+
+  /** The list of scheduled event calls. */
+  private events: TimerEvent[] = [];
+
+  /** The current index of the next chronological event waiting to be called. */
+  private eventIdx = 0;
+
+  /** True if the events list needs to be sorted. */
+  private dirty = false;
+
+  /** An event called every iteration of some time interval. This interval is always from time=0. */
+  private everyEvent?: TimerEvent;
+
+  /** The next clock time the every-event will be called at. */
+  private everyNextCallTime = 0;
+
+  /** Whether time=0 should be included in the list of every-event pulses. */
+  private everyOnStart = false;
+
+  /** Update function for tween effects. */
+  private tweenFunc?: (n: number) => void;
+
+  /** Context for the tween function call. */
+  private tweenContext?: object;
+
+  /** Shape function for passed values to the tween update function. */
+  private tweenShape = (n: number) => n;
+
+  /** Flag signal that tween is done and can cease functioning.
+   * Used to ensure that a final tween=1.0 is called to complete the effect. */
+  private tweenFinished = false;  // TODO Roll this into something of higher scope?
+
+
+  constructor(seconds?: number, event?: () => void, context?: object) {
+    this.at(seconds || 0, event || Timer.NULL_EVENT, context);
     Game.scene.ticker.add(this.update, this);
   }
 
   destroy() {
     Game.scene.ticker.remove(this.update, this);
+    // TODO Deconstruct all event calls, too
   }
 
+  /** The timer's elapse length in milliseconds. This is always reflective of the timestamp of the last scheduled event. */
+  private get lengthMillis() {
+    this.sortEvents();
+    return this.events[this.events.length-1].time;
+  }
+
+  /** The timer's elapse length in seconds. This is always reflective of the timestamp of the last scheduled event. */
+  get length() {
+    return this.lengthMillis * TO_SECONDS;
+  }
+
+  /** Starts the timer's clock. */
   start() {
-    this._started = true;
+    this.sortEvents();
+    this.started = true;
   }
 
+  /** Resets the timer's clock, then starts it. */
   startReset() {
     this.reset();
     this.start();
   }
 
+  /** Stops the timer's clock. May be resumed with start(). */
   stop() {
-    this._started = false;
+    this.started = false;
   }
 
+  /** Stops and reset the timer's clock. */
   stopReset() {
     this.reset();
     this.stop();
   }
 
+  /** Resets the timer's clock and scheduled event calls to initial. Does not stop the clock. */
   reset() {
-    this.elapsedTime = 0;
+    this.elapsedMillis = 0;
+    this.eventIdx = 0;
+    this.everyNextCallTime = (!this.everyOnStart)
+      ? this.everyEvent?.time || 0
+      : 0;
+    this.tweenFinished = false;
   }
 
+  /** The timer's elapsed clock time in seconds. */
   get elapsed() {
-    return this.elapsedTime;
+    return this.elapsedMillis * TO_SECONDS;
   }
 
+  /** True if the timer is start and hasn't yet finished. */
   get ticking() {
-    return (this._started && this.elapsedTime < this.timerLength);
+    return (this.started && this.elapsedMillis < this.lengthMillis);
   }
 
+  /** True if the timer has finished. */
   get finished() {
-    return (this.elapsedTime >= this.timerLength);
+    return (this.elapsedMillis >= this.length);
   }
 
+  /** True if all timer events have been called. */
+  private get eventsExhausted() {
+    return this.eventIdx >= this.events.length;
+  }
+
+  /** The current event the timer is waiting to call. */
+  private get currentEvent() {
+    return (this.eventIdx < this.events.length)
+      ? this.events[this.eventIdx]
+      : Timer.NULL_SCHEDULE;
+  }
+
+  /** Handles timer management. */
   private update() {
-    if (!this._started)
+    if (!this.started)
       return;
 
-    if (this.finished) {
-      if (this.callback)
-        this.callback();
-      this.stop();
+    this.elapsedMillis += Game.deltaMS;
+    
+    // Force chronological order — this goes through a 'dirty' check, so it shouldn't
+    // affect performance. It's just here to clean up during-runtime event additions.
+    this.sortEvents();
+
+    // Initiate event callbacks
+    this.handleEvents();
+    this.handleEveryEvent();
+    this.handleTweenEvent();
+
+    if (this.finished && this.selfDestruct)
+      this.destroy();
+  }
+
+  /** Manages at-event calls. */
+  private handleEvents() {
+    if (this.eventsExhausted)
+      return;
+    while (this.currentEvent.time <= this.elapsedMillis) {
+      this.currentEvent.event.call(this.currentEvent.context);
+      this.eventIdx++;
     }
-    else {
-      this.elapsedTime += Game.deltaMS;
+  }
+
+  /** Manages every-event calls. */
+  private handleEveryEvent() {
+    if (!this.everyEvent)
+      return;
+    while (this.elapsedMillis >= this.everyNextCallTime) {
+      this.everyEvent.event.call(this.everyEvent.context);
+      this.everyNextCallTime += this.everyEvent.time;
     }
   }
 
-  // autoDestroy = true
-  // This sets the timer to destroy itself after the last .at() call, or its max time.
-  // If this is not true, or if there is an .every(), this timer cannot destroy itself unless a max time is given.
+  /** Manages tween-event calls. */
+  private handleTweenEvent() {
+    if (!this.tweenFunc || this.tweenFinished)
+      return;
 
-  at(time: number, cb: () => void, context?: object) {
-    // schedules an event at some time value.
+    const { min } = Math;
+    const timerProgress = min(this.elapsedMillis / this.length, 1);
+    const n = this.tweenShape(timerProgress);
+    this.tweenFunc.call(this.tweenContext, n);
+
+    if (timerProgress === 1)
+      this.tweenFinished = true;
   }
 
-  every(options: {interval: number, from?: number, firstInterval?: number, onStart?: boolean}, cb: () => void, context?: object) {
-    // Schedules an event at every interval of some time value since the starting value.
-    // This is Pulsar behavior, so I don't know if I care about implementation here.
-
-    // interval       : time between calls
-    // firstInterval  : can be different from regular interval; cursor behavior
-    // from           : from when the interval starts counting; from is the first pulse time
-    // onStart        : whether to pulse on time == 0; this is absolute and unaffected by 'from'!
-
-    // from = from || 0
-    // from2 = from + firstInterval
-    // firstTime = clock - from
-    // secondTime = firstTime - from2
-    //
-    // firstPulse = (from !== 0 && firstTime === 0)
-    // secondPulse = (secondTime === 0)
-    // intervalTicking = (secondTime > 0)
-    // intervalPulse = (secondTime % interval === 0)
-    // startPulse = (clock === 0 && onStart)
-    // if (intervalTicking && intervalPulse || startPulse || firstPulse || secondPulse)
-
-    // Okay. Comment code. I didn't mean to go that hard.
-    // I think, with few mistakes, that should cover pulses at these times:
-    //  - time = 0      if onStart
-    //  - time = from   if from > 0
-    //  - time = from + firstInterval
-    //  - time = from + firstInterval + interval
-    //  - time = from + firstInterval + interval + ...
-    // If not, I guess I can just follow this chart I just wrote.
+  /** Mutates the events schedule to be in chronological order. If the clock was somehow
+   * started before the events were sorted, previously called events may be called again. */
+  sortEvents() {
+    if (!this.dirty)
+      return this;
+    const cur = this.currentEvent;
+    this.events.sort( (a,b) => a.time - b.time );
+    this.eventIdx = this.events.findIndex( e => e === cur );
+    return this;
   }
 
-  tween(time: {length: number, at?: number}, cb: (slider: Slider) => void, context?: object) {
-    // for time 'length', call the cb function
-    // cb gets passed a slider whose track position is moderated by the time interval.
-    // Slider options should be an optional parameter
-
-    // Did I think about implementation at all?
-    // How... hold on.
-
-    // timer = new Timer(2);      // 2 seconds
-    // timer.tween({
-    //   object,
-    //   prop: 'x',         // What would be the best way to pass in a property to tweak?
-    //   from?: obj.x       // 'from' is current by default
-    //   to?: obj.x + 120,  // 'to' is current by default (convenience)
-    //   shape?: v => Math.pow(v, 2),   // linear by default
-    //   operator?: slider => {}
-    // });
-    //
-    // This part is implicit:
-    //   f: slider => { obj.x = slider.output*(end-start) + start; },
-    //
-    // Ex:
-    // new Timer(1.2).tween({
-    //   object: illustration,
-    //   prop: 'x',
-    //   to: -64,
-    // });
-    // 
-    // For 1.2 seconds, move 'illustration' from its current x to -64,
-    // auto deconstruct tween object afterward.
-    //
-    // If I wanted to use tween() to manage a spiral-in animation, how would I do that?
-    // Maybe I should keep the slider cb?
-    // Yeah. I was trying to reduce the boilerplate, but I think for the purpose of
-    // intricate animation I want to be able to directly state how the slider output
-    // is applied to anything.
-
-    // I gotta get back to my day job.
-
-    // Timer.tween(3, {
-    //   shape?: v => number,
-    //   f: slider => {},
-    //   context?: object,
-    // })
-    //
-    // Okay. This works.
-    // tween is a static method. It creates a new timer object of 3 seconds, does
-    // the stuff and auto-destructs it after its finished. A reference is returned
-    // in case you want it to skip or destroy early.
-    // Should it return a reguler Timer object? What if you .at()?
-    // Actually, what if you .at()?
-    //
-    // Timer.tween(3, {...})
-    //   .at(1.5, () => { particle explosion })
-    //   .at(2.0, () => { particle explosion })
-    //
-    // I don't know... how I want that to work under the hood, but wouldn't that
-    // be cool? A naive user might want their tween effect to be a child of a longer
-    // timer, but I think I need to just define it strictly as an 'of-length' effect
-    // for simplicity. You can .at(2, Timer.tween()) on a higher-order timer if you
-    // really need that, which is what I planned from the beginning anyway.
+  /** Tells the timer not to destroy itself on finish; returns this. */
+  noSelfDestruct() {
+    this.selfDestruct = false;
+    return this;
   }
+
+  /** Schedules an event-call at some time value; returns this. */
+  at(time: number, event: () => void, context?: object) {
+    time *= TO_MILLIS;
+    const timerEvent = {time, event, context};
+    this.events.push(timerEvent);
+    return this;
+  }
+
+  // removeEvent(event: () => void, context?: object) { }
+  // TODO Should I?
+  // Seems easier to just have a positive-only construction style.
+  // Like, how much fiddling is reasonable?
+  // These are supposed to be set-and-forget, you know.
+
+  /** Schedules an event-call every 'time' seconds; returns this.
+   * Every-events are by nature infinite and must be destroyed manually. */
+  every(time: number, event: () => void, context?: object, onStart: boolean = false) {
+    time *= TO_MILLIS;
+    const timerEvent = {time, event, context};
+    this.everyEvent = timerEvent;
+    this.everyNextCallTime = timerEvent.time;
+    this.everyOnStart = onStart;
+    return this;
+
+    // TODO Timer.at(5, { Timer.every(3, {...}, onStart=true) })
+    // How does Timer.every() inside the Timer.at() get memory cleared?
+    // We literally can't interact with it.
+    // I think passing in Timers as children instead of event functions, like as an
+    // overload alternative, would allow child-timer management that could still
+    // interact with such things. I don't know, though.
+    //
+    // Timer.at(5, Timer.every(3, {...}));  ← Pulsar with delayed start. Ideally.
+    //
+    // Should every() force you to declare max calls?
+    // You could answer 'infinite', but I think psychologically this could enable
+    // the realization that infinite everys can't clean themselves up.
+    // Allowing Timers instead of void functions is great, but it doesn't stop
+    // people from writing an every() inside a void function.
+  }
+
+  /** Schedules a progressive event call for the duration of the timer; returns this.
+   * Timer duration is always reflective of its last chronological event, so be mindful
+   * that Timer(time) is deliberate and knowingly overridden. */
+  tween(iter: (n: number) => void, context?: object, shape?: (n: number) => number) {
+    this.tweenFunc = iter;
+    this.tweenContext = context;
+    (shape) && (this.tweenShape = shape);
+    return this;
+  }
+
 }
