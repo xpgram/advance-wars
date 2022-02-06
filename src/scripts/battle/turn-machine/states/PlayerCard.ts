49ae45d8a731124447512822df3c809665df61c0
Devin Biddy <xp.gram@gmail.com>
2022-02-06 14:52:56 -0800

    use palette swap instead of hue rotate / tweak positions

--- src/scripts/battle/turn-machine/states/PlayerCard.ts
+++ src/scripts/battle/turn-machine/states/PlayerCard.ts
@@ -1,12 +1,61 @@
-import { ColorOverlayFilter } from 'pixi-filters';
+import { MultiColorReplaceFilter } from 'pixi-filters';
 import { Game } from "../../../..";
 import { Ease } from "../../../Common/EaseMethod";
 import { ImmutablePointPrimitive, Point } from "../../../Common/Point";
-import { NumericDictionary } from "../../../CommonTypes";
 import { Timer } from "../../../timer/Timer";
-import { Faction } from "../../EnumTypes";
 import { fonts } from "../../ui-windows/DisplayInfo";
 import { TurnState } from "../TurnState";
+
+
+const paletteSwaps = [
+  [ // Red → Red
+    [0x000000, 0x000000],
+  ],
+  [ // Red → Blue
+    [0x633131, 0x29425A],
+    [0xF7B5B5, 0xC6DEDE],
+    [0xDE8484, 0x7BADC6],
+    [0xEFA5A5, 0xA5D6D6],
+    [0xCE7373, 0x6B9CB5],
+    [0xBD6B6B, 0x528CAD],
+    [0xFFDEDE, 0xEFEFFF],
+    [0xEF9C9C, 0x94BDCE],
+    [0x7B4242, 0x29526B],
+    [0xAD5A5A, 0x427B94],
+    [0xFFC6C6, 0xD6DEEF],
+    [0x8C4A4A, 0x316384],
+  ],
+  [ // Red → Yellow
+    [0x633131, 0x845A18],
+    [0xF7B5B5, 0xF7E79C],
+    [0xDE8484, 0xE7CE73],
+    [0xEFA5A5, 0xEFDE8C],
+    [0xCE7373, 0xDEBD5A],
+    [0xBD6B6B, 0xD6BD42],
+    [0xFFDEDE, 0xFFF7C6],
+    [0xEF9C9C, 0xEFDE8C],
+    [0x7B4242, 0x8C6B29],
+    [0xAD5A5A, 0xBD9C39],
+    [0xFFC6C6, 0xF7E7AD],
+    [0x8C4A4A, 0xA58C31],
+    [0xFFFFFF, 0xFFFFD6],
+  ],
+  [ // Red → Black
+    [0x633131, 0x313931],
+    [0xF7B5B5, 0xA5ADA5],
+    [0xDE8484, 0x848C84],
+    [0xEFA5A5, 0x9CA59C],
+    [0xCE7373, 0x738473],
+    [0xBD6B6B, 0x737B73],
+    [0xFFDEDE, 0xBDBDBD],
+    [0xEF9C9C, 0x9CA59C],
+    [0x7B4242, 0x4A4A4A],
+    [0xAD5A5A, 0x636B63],
+    [0xFFC6C6, 0xADB5AD],
+    [0x8C4A4A, 0x525252],
+    [0xFFFFFF, 0xC6C6C6],
+  ],
+];
 
 
 /** Shows the current player's begin-turn animation sweep. */
@@ -30,70 +79,25 @@ export class PlayerCard extends TurnState {
     const point = (x: number, y: number) => new Point(x,y);
     const hide = 200;
 
-    const recolor = [
-      [ // Red
-        new ColorOverlayFilter(0xFF1400, .235),
-        (function() {
-          const f = new PIXI.filters.ColorMatrixFilter();
-          f.contrast(.20);
-          f.brightness(.9, true);
-          return f;
-        })(),
-      ],
-      [ // Blue
-        new ColorOverlayFilter(0x1800FF, .15),
-        (function() {
-          const f = new PIXI.filters.ColorMatrixFilter();
-          f.contrast(.15);
-          f.brightness(.9, true);
-          return f;
-        })(),
-      ],
-      [ // Yellow
-        new ColorOverlayFilter(0xFFDD00, .35),
-        (function() {
-          const f = new PIXI.filters.ColorMatrixFilter();
-          f.contrast(.30);  // TODO ..? No effect?
-          f.brightness(.8, true);
-          return f;
-        })(),
-      ],
-      [ // Black
-        new ColorOverlayFilter(0x330022, .35),
-        (function() {
-          const f = new PIXI.filters.ColorMatrixFilter();
-          f.contrast(.20);
-          // f.brightness(.9);
-          return f;
-        })(),
-      ],
-    ]
-
     // Construct temp player card.
 
     // Insig  - 1
     const insignia = new PIXI.Container();
     
     const splash = new PIXI.Sprite(players.current.officer.insigniaSplash);
-    splash.filters = recolor[players.current.playerNumber];
+    const playerColor = players.current.playerNumber;
+    splash.filters = [new MultiColorReplaceFilter(paletteSwaps[playerColor])];
     splash.position.set(rw*.75, rh*.5);
+    splash.cacheAsBitmap = true;
 
-    const splashShadow = new PIXI.Sprite(players.current.officer.insigniaSplash);
-    splashShadow.filters = [
-      new ColorOverlayFilter(0, 1),
-      new PIXI.filters.BlurFilter(8.0),
-    ];
-    splashShadow.position.set(splash.x, splash.y + 2);
-    splashShadow.alpha = .75;
-
-    insignia.addChild(splashShadow, splash);
+    insignia.addChild(splash);
     insignia.alpha = 0;
 
     // Day    - 1
     const dayCard = this.newRect({
       g: new PIXI.Graphics(),
       dim: point(24,16),
-      pos: point(-hide, rh*.25),
+      pos: point(-hide, rh*.20),
     });
     const dayText = new PIXI.BitmapText('Day', fonts.title);
     dayText.anchor.set(.5);
@@ -104,7 +108,7 @@ export class PlayerCard extends TurnState {
     const dayNumCard = this.newRect({
       g: new PIXI.Graphics(),
       dim: point(32,32),
-      pos: point(-hide, rh*.425),
+      pos: point(-hide, rh*.39),
     })
     const dayNumText = new PIXI.BitmapText(`${players.day}`, fonts.title);
     dayNumText.anchor.set(.5);
@@ -114,20 +118,20 @@ export class PlayerCard extends TurnState {
     // Map    - 1.5
     const mapCard = this.newRect({
       g: new PIXI.Graphics(),
-      dim: point(128,16),
-      pos: point(-hide, rh*.625),
+      dim: point(256,16),
+      pos: point(-256, rh*.60),
     })
     mapCard.pivot.x = 0;
     const mapText = new PIXI.BitmapText(`Land's End`, fonts.script);
     mapText.anchor.set(.5,.5);
-    mapText.position.set(mapCard.width*.65, mapCard.height/2);
+    mapText.position.set(mapCard.width*.825, mapCard.height/2);
     mapCard.addChild(mapText);
 
     // Fight  - 3
     const fightCard = this.newRect({
       g: new PIXI.Graphics(),
       dim: point(64,24),
-      pos: point(-hide, rh*.8),
+      pos: point(-hide, rh*.77),
     })
     const fightText = new PIXI.BitmapText(`Fight!`, fonts.title);
     fightText.anchor.set(.5);
@@ -137,7 +141,7 @@ export class PlayerCard extends TurnState {
     // Left-side mover
     const driftContainer = new PIXI.Container();
     driftContainer.addChild(dayCard, dayNumCard, fightCard);
-    driftContainer.x = rw*.20;
+    driftContainer.x = rw*.25 - 8;
 
     // Destructable
     this.container = new PIXI.Container();
@@ -160,7 +164,7 @@ export class PlayerCard extends TurnState {
       .tween(driftTime, driftContainer, {x: driftContainer.x + 16}) //, Ease.quantize(Ease.linear.out, 16))
       .tween(insigniaFadeTime, insignia, {alpha: 1}, Ease.sine.inOut)
       .tween(slideTime, dayCard, {x: 0}, motion.out)
-      .tween(mapCardTime, mapCard, {x: -8}, Ease.sine.inOut)
+      .tween(mapCardTime, mapCard, {x: -128}, Ease.sine.inOut)
 
       .wait(delay)
       .tween(slideTime, dayNumCard, {x: 0}, motion.out)
