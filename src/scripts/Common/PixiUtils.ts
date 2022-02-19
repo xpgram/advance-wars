22b8993c8167f22113d3846f1f45500628eb5d33
Devin Biddy <xp.gram@gmail.com>
2022-02-19 12:57:22 -0800

    add function / doc strings

--- src/scripts/Common/PixiUtils.ts
+++ src/scripts/Common/PixiUtils.ts
@@ -1,10 +1,11 @@
 import { Game } from "../..";
+import { Debug } from "../DebugUtils";
 
-/**  */
+/** Common functions to be used with the Pixi WebGL framework. */
 export module PixiUtils {
 
-  /**  */
-  export function SnapshotContainerObject(container: PIXI.Container): PIXI.Texture {
+  /** Returns a rendered texture of the given container object. */
+  export function snapshotContainerObject(container: PIXI.Container): PIXI.Texture {
     const canvas = PIXI.RenderTexture.create(
       container.x + container.width,
       container.y + container.height,
@@ -15,6 +16,17 @@ export module PixiUtils {
       }
     );
     return canvas;
+  }
+
+  /** Mutates the given BitmapText object so its text field is no longer than the given max pixel width.
+   * Returns the mutated BitmapText object. */
+  export function limitBitmapTextToWidth(gtext: PIXI.BitmapText, maxWidth: number): PIXI.BitmapText {
+    Debug.assert(maxWidth >= 1, `Width limit cannot be less than 1 pixel.`);
+
+    while (gtext.width > maxWidth)
+      gtext.text = gtext.text.slice(0,-1);
+
+    return gtext;
   }
 
 }
