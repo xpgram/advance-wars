2bac60e839bde16fc2832378bfdbdf7cb7f79546
Dei Valko <xp.gram@gmail.com>
2022-02-24 13:07:22 -0800

    add basic capture animation

--- src/scripts/battle/map/tile-effects/CapturePropertyEvent.ts
+++ src/scripts/battle/map/tile-effects/CapturePropertyEvent.ts
@@ -1,3 +1,6 @@
+import { Game } from "../../../..";
+import { Rectangle } from "../../../Common/Rectangle";
+import { Timer } from "../../../timer/Timer";
 import { BattleSceneControllers } from "../../turn-machine/BattleSceneControllers";
 import { UnitObject } from "../../UnitObject";
 import { TerrainObject } from "../TerrainObject";
@@ -19,11 +22,10 @@ export class CapturePropertyEvent extends TileEvent {
     this.options = options;
   }
 
-  protected ratify(): void {
+  private captureProperty(): void {
     const { map, players } = this.options.assets;
     const { actor, terrain } = this.options;
 
-    actor.captureBuilding();
     if (actor.buildingCaptured()) {
       actor.stopCapturing();
       terrain.faction = actor.faction;
@@ -31,31 +33,78 @@ export class CapturePropertyEvent extends TileEvent {
     }
   }
 
+  protected ratify(): void {
+    const { actor } = this.options;
+
+    actor.captureBuilding();
+    this.captureProperty();
+  }
+
   protected create(): void {
     const { actor } = this.options;
 
-    this.ratify();
-    this.finish();
-    return;
-
+    // Ratify immediate changes; record changes for animation
     const preCapture = actor.capture;
     actor.captureBuilding();
     const postCapture = actor.capture;
 
-    const bg = new PIXI.Graphics();
-    bg.beginFill(0);
-    bg.drawRect(0,0,128,128);
-    bg.endFill();
+    // Setup UI element definitions
+    const windowRect = new Rectangle(0,0,80,64);
 
-    const fill = new PIXI.Graphics();
-    fill.beginFill(0xFFFFFF);
-    fill.drawRect(0,0,24,24);
-    fill.endFill();
+    const barSep = 3;
+    const bar = new Rectangle(
+      windowRect.width-18,
+      windowRect.height-barSep-1,
+      16,
+      barSep-1
+    );
 
-    const setValue = (n: number) => {
-      const r = n / 20;
-
+    const drawRect = (g: PIXI.Graphics, r: Rectangle, c: number, a: number) => {
+      g.beginFill(c,a);
+      g.drawRect(r.x, r.y, r.width, r.height);
+      g.endFill();
     }
+
+    // Assemble UI elements
+    const bg = new PIXI.Graphics();
+    drawRect(bg, windowRect, 0, .5);
+    bg.position.set(
+      Game.display.renderWidth/2,
+      Game.display.renderHeight/2,
+    )
+    bg.pivot.set(
+      bg.width/2,
+      bg.height/2,
+    )
+
+    const meter = new PIXI.Graphics();
+    for (let i = 0; i < 20; i++)
+      drawRect(meter, bar.move(0, -barSep*i), 0x444444, 1);
+
+    const meterFill = new PIXI.Graphics();
+    for (let i = 0; i < preCapture; i++)
+      drawRect(meterFill, bar.move(0, -barSep*i), 0x888888, 1);
+    
+    // Add to scene
+    bg.addChild(meter, meterFill);
+    Game.hud.addChild(bg);
+
+    // Animation schedule
+    const timer = Timer.at(.15);
+    
+    let timeSep = .05;
+    for (let i = preCapture; i < postCapture; i++) {
+      timer
+        .do(n => drawRect(meterFill, bar.move(0, -barSep*i), 0x888888, 1))
+        .wait(timeSep);
+    }
+
+    timer
+      .at('end')
+      .wait(.5)
+      .do(this.captureProperty, this)
+      .do(n => bg.destroy({children: true}))
+      .do(this.finish, this);
   }
 
   protected update(): void {
