9bc876c8a7fe9e4bc2b38acead2ed0c6fe7232ec
Dei Valko <xp.gram@gmail.com>
2022-02-24 18:02:44 -0800

    create globally accessible palette information (stub)

---
+++ src/scripts/battle/ui-windows/ColorPalette.ts
@@ -0,0 +1,28 @@
+
+
+export const palettes = {
+  ui: {
+
+  },
+  factions: {
+    red: {
+      primary: 0,
+      insigniaSplash: [
+        // This is supposed to be the palette swap info from red to ???
+        // However, either I need to describe red here as well, which seems silly,
+        // or I need to be *absolutely sure* that the order of these color
+        // listings match those of ???'s.
+        // Maybe I'll just call it insigniaSplashPaletteSwap and do the obvious thing.
+      ],
+    },
+    blue: {
+
+    },
+    yellow: {
+
+    },
+    black: {
+
+    },
+  },
+}
