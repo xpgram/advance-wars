47c580b3984b2e9104bb7f65ebfb52ee64a83a4c
Dei Valko <xp.gram@gmail.com>
2022-03-01 14:05:19 -0800

    update todo

--- src/scripts/color-palette/ColorPalette.ts
+++ src/scripts/color-palette/ColorPalette.ts
@@ -1,11 +1,28 @@
 
-// TODO Move this to src/Common, or maybe just src
+// TODO This is way too specific.
+// Accessing the color for a popup window via
+// palettes.ui.window.background *inside* that popup window seems a little odd.
+//
+// Like, CommandMenu *should* be responsible for its own appearance, right?
+// On some level.
+// So why is it defined here?
+// Because it might be reused.
+// Reused by what, and for what purpose?
+// I need language that answers *this* question; palettes.ui.window.background
+// fails to meaningfully address the palette problem, it just moves the same
+// structure in CmdMenu to here, and it ends up all the more confusing for it.
+//
+// I'll need to do some thinking.
+// A little CSS influence might not hurt.
 
 import { PlayerFactionPalette } from "./PlayerFactionPalette";
 
 export const palettes = {
   ui: {
-
+    window: {
+      background: 2,
+      cursor: 2,
+    }
   },
   factions: {
     red: <PlayerFactionPalette>{
