e53dd009ff5ab71c555ea7205bcf8d85d478e6fc
Dei Valko <xp.gram@gmail.com>
2022-02-28 16:19:47 -0800

    add (unused) auto-create-new PIXI.Graphics

--- src/scripts/Common/PixiGraphics.ts
+++ src/scripts/Common/PixiGraphics.ts
@@ -7,8 +7,16 @@ type Graphics = PIXI.Graphics;
  * Provides shortcuts to drawing common shapes and effects. */
 export module PixiGraphics {
 
+  /** Returns the given, or a new, PIXI.Graphics object.
+   * Useful to confirm a real object is always being manipulated. */
+  function confirmGraphicsObject(g?: Graphics): Graphics {
+    return g ?? new PIXI.Graphics();
+  }
+
   /** Draws a rectangle with the given Graphics object. */
   export function drawRect(g: Graphics, rect: RectanglePrimitive, color: number, alpha?: number): Graphics {
+    // g = confirmGraphicsObject(g);
+      // TODO How should g be optional? It's the first parameter.
     g.beginFill(color, alpha ?? 1);
     g.drawRect(rect.x, rect.y, rect.width, rect.height);
     g.endFill();
