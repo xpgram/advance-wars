233d7c8d06219f3b9fb3cc1e48e0e49f5db1bc7c
Dei Valko <xp.gram@gmail.com>
2022-03-02 13:46:28 -0800

    expand palette; update todo

--- src/scripts/color/PlayerFactionPalette.d.ts
+++ src/scripts/color/PlayerFactionPalette.d.ts
@@ -2,8 +2,24 @@ import { HexColor } from "./ColorTypes";
 
 /**  */
 export type PlayerFactionPalette = {
-  menu: {
-    primary: HexColor,
-    // secondary: HexColor,
+  CoWindow: {
+    background: HexColor,
+  }
+  propertyCapture: {
+    meter: HexColor,
+    tint: HexColor,
+  }
+  turnStartSplash: {
+    // TODO I think I just use swap pairs, here.
+    // This might be irrelevant.
+  }
+  paletteSwap: {
+    // TODO ?
+    // This would be from red.
+    // This would be useful if I can consolidate the different color-
+    // pair-sets I have for different images into one mega pair-set.
+    // If I can confirm there are no duplicate answers, or that they
+    // are minimal and segregatable into purposes (like troops, ui, etc.),
+    // then this is a good place to keep that data.
   }
 }
