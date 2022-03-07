0ce2c1e34f748f9b1de20be55d1f86fa595a4379
Dei Valko <xp.gram@gmail.com>
2022-03-07 13:07:29 -0800

    refactor to module pattern

--- src/scripts/CommonUtils.ts
+++ src/scripts/CommonUtils.ts
@@ -4,49 +4,49 @@ import { Debug } from "./DebugUtils";
 /**
  * Commonly useful functions.
  * @author Dei Valko
- * @version 0.1.2
+ * @version 0.1.4
  */
-export const Common = {
+export module Common {
 
   /** Freezes the given object and returns it. */
-  freezeObject<T>(obj: T): T {
+  export function freezeObject<T>(obj: T): T {
     Object.freeze(obj);
     return obj;
-  },
+  }
 
   /** Given a set of options and a set of defaults, returns an object  */
-  assignDefaults<T extends {}, Y extends {}>(options: T, defaults: Y): T & Y {
+  export function assignDefaults<T extends {}, Y extends {}>(options: T, defaults: Y): T & Y {
     const res: Record<string, any> = {};
     const A = options as Record<string, any>;   // Literally just to make TypeScript shut up.
     const B = defaults as Record<string, any>;
     Object.keys({...options, ...defaults})
       .forEach( key => { res[key] = (A[key] !== undefined) ? A[key] : B[key] });
     return res as T & Y;
-  },
+  }
 
   /** Given an object, completely dismantles its assigned properties.
    * This iterates over every property key, which may be a point of optimization.
    * This does not offer any depth dismantling; members are forgotten but otherwise intact. */
-  destroyObject<T extends {}>(object: T): void {
+  export function destroyObject<T extends {}>(object: T): void {
     const obj = object as Record<string, any>;
     Object.keys(object).forEach( key => obj[key] = undefined);
-  },
+  }
 
   /** Returns a two-dimensional array filled with sleep. // TODO */
-  Array2D<T>(rows: number, columns: number, fill: (() => T) | T): T[][] {
+  export function Array2D<T>(rows: number, columns: number, fill: (() => T) | T): T[][] {
     let cb = (fill instanceof Function) ? fill : () => fill;
     return Array.from<any, T[]>(Array(rows), () => Array.from<any, T>(Array(columns), cb));
-  },
+  }
 
   /** Given a list of things, returns a random thing. */
-  pick<T>(list: T[]): T {
+  export function pick<T>(list: T[]): T {
     const { random, floor } = Math;
     return list[ floor(random()*list.length) ];
-  },
+  }
 
   /** Confines n to the range [min, max] inclusive.
    * @deprecated clamp() is more descriptive. I think. */
-  confine(n: number, min: number, max: number) {
+  export function confine(n: number, min: number, max: number) {
     if (min > max) {
       let tmp = min;
       min = max;
@@ -55,69 +55,69 @@ export const Common = {
     if (n < min) n = min;
     else if (n > max) n = max;
     return n;
-  },
+  }
 
   /** Contrains n to the range [min, max] inclusive. */
-  clamp(n: number, min: number, max: number) {
+  export function clamp(n: number, min: number, max: number) {
     if (max < min)
       throw new Error(`min cannot be greater than max`);
     return Math.min(max, Math.max(min, n));
-  },
+  }
 
   /** Contains `n` to the range [0, `lim`] by circularly projecting the
    * the space (-∞,∞) onto the range defined by `lim`. Useful for containing
    * degrees to the range [0,360], for instance. */
-  rotate(n: number, lim: number) {
+  export function rotate(n: number, lim: number) {
     n %= lim;
     return (n >= 0) ? n : n + lim;
-  },
+  }
 
   /** Returns true if n is in the range [min,max], default inclusive. */
-  within(n: number, min: number, max: number, exclusive?: boolean) {
+  export function within(n: number, min: number, max: number, exclusive?: boolean) {
     if (exclusive)  // By default, undefined
       return (n > min && n < max);
     else
       return (n >= min && n <= max);
-  },
+  }
 
   /** Returns true if n is in the range [min,max], default inclusive. */
-  inRange(n: number, min: number, max: number, exclusive?: boolean) {
+  export function inRange(n: number, min: number, max: number, exclusive?: boolean) {
     return (exclusive)
       ? min < n && n < max
       : min <= n && n <= max;
-  },
+  }
 
   /** Returns true if n is in the range [0,length), inclusive-exclusive. */
-  validIndex(n: number, length: number) {
+  export function validIndex(n: number, length: number) {
     return Common.inRange(n, 0, length-1);
-  },
+  }
 
   /** Returns -1 if n is less than min, 1 if more than max, and 0 if between.
    * The range [min, max] is inclusive. */
-  rangeDisplacementSide(n: number, min: number, max: number): number {
+  export function rangeDisplacementSide(n: number, min: number, max: number): number {
     return -1*Number(n < min) + Number(n > max);
-  },
+  }
 
   /** Returns the relative distance n is from the inclusive range [min, max].
    * Returned number's range is [-distance, 0, +distance] */
-  displacementFromRange(n: number, min: number, max: number): number {
+  export function displacementFromRange(n: number, min: number, max: number): number {
     return (n - min)*Number(n < min) +
            (n - max)*Number(n > max);
-  },
+  }
 
   /** Given a list of objects with a weight property, returns a copy of
    * that list sorted by that property in ascending order. */
-  sortByWeight(li: {weight: number}[]) {
+  export function sortByWeight(li: {weight: number}[]) {
     return li.slice().sort( (a,b) => a.weight - b.weight );
-  },
+  }
 
   /** Returns true if box1 and box2 overlap in any way. */
-  boxCollision(box1: PIXI.Rectangle, box2: PIXI.Rectangle) {
+  export function boxCollision(box1: PIXI.Rectangle, box2: PIXI.Rectangle) {
     return (box1.x < box2.x + box2.width &&
       box1.x + box1.width > box2.x &&
       box1.y < box2.y + box2.height &&
       box1.y + box1.height > box2.y);
-  },
+  }
 
   /**
    * Retrieves bits from the object's information number. All JS numbers are 64-bit.
@@ -127,10 +127,10 @@ export const Common = {
    * @return A number equivalent to the bits retrieved from the given store value.
    * @deprecated Use BitIO
    */
-  readBits(store: number, length: number, shift: number) {
+  export function readBits(store: number, length: number, shift: number) {
     let mask = Math.pow(2, length) - 1;  // Get us a series of 1 bits.
     return (store >> shift & mask);
-  },
+  }
 
   /**
    * Writes bits to the object's information number. All JS numbers are 64-bit.
@@ -141,47 +141,47 @@ export const Common = {
    * @param shift How far left the bit-mask is applied.
    * @deprecated Use BitIO
    */
-  writeBits(store: number, value: number, length: number, shift: number) {
+  export function writeBits(store: number, value: number, length: number, shift: number) {
     let mask = Math.pow(2, length) - 1;  // Get us a series of 1 bits.
     store = store & ~(mask << shift);
     store += (value & mask) << shift;
     return store;
-  },
+  }
 
   /** Given a list of things, return a 'bouncing loop' of that list's contents, triangle-wave-style.
    * Ex: Given the list [1,2,3,4,5], return [1,2,3,4,5,4,3,2].
    */
-  listToBouncingLoop(list: Array<any>) {
+  export function listToBouncingLoop(list: Array<any>) {
     let result = list.slice();
     for (let i = list.length - 2; i > 0; i--)
       result.push(list[i]);
     return result;
-  },
+  }
 
   /** Given the frames.length of an animation and the desired time to completion (seconds), returns the fraction
    * of frames to play per global update (a Pixi.js thing) as a number.
    */
-  animationSpeedFromTimeDuration(animLength: number, timeDuration: number) {
+  export function animationSpeedFromTimeDuration(animLength: number, timeDuration: number) {
     return animLength / (60 * timeDuration);
     // TODO Get 60 fps from Pixi.ticker settings?
-  },
+  }
 
   /** Given the frames.length of an animation and the speed at which it plays (frames per frame), returns the
    * time-length of the animation in seconds. */
-  animationTimeDuration(animLength: number, animationSpeed: number) {
+  export function animationTimeDuration(animLength: number, animationSpeed: number) {
     return animLength / (60 * animationSpeed);
-  },
+  }
 
   /** Returns the reciprocal of the elapsed frames desired per frame-change.
    * Providing this a value of 15 returns a number corresponding to one frame-change per 1/4th a second.
    */
-  animationSpeedFromFrameInterval(interval: number) {
+  export function animationSpeedFromFrameInterval(interval: number) {
     return 1 / interval;
-  },
+  }
 
   /** Returns the first found repeating sequence within the list li.
    * A sequence must repeat 3 times for it to be returned. */
-  repeatingSequence<T>(li: T[], maxSequence: number): T[] {
+  export function repeatingSequence<T>(li: T[], maxSequence: number): T[] {
     function get(idx: number): T | undefined {
       if (Common.validIndex(idx, li.length))
         return li[idx];
@@ -211,6 +211,6 @@ export const Common = {
     }
     // Null case
     return [];
-  },
+  }
 
 }
