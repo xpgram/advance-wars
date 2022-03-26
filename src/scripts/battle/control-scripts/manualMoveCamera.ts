8386a9f857c691a34b3114365da670c1517c6bcc
Dei Valko <xp.gram@gmail.com>
2022-03-25 17:10:16 -0700

    fix (?) camera flyaway manual movement

--- src/scripts/battle/control-scripts/manualMoveCamera.ts
+++ src/scripts/battle/control-scripts/manualMoveCamera.ts
@@ -2,6 +2,7 @@ import { Game } from "../../..";
 import { Point } from "../../Common/Point";
 import { PositionContainer } from "../../CommonTypes";
 import { Common } from "../../CommonUtils";
+import { Keys } from "../../controls/KeyboardObserver";
 import { ControlScript } from "../../ControlScript";
 
 
@@ -42,9 +43,15 @@ export class ManualMoveCamera extends ControlScript {
       (dpad.point.y !== 0) ? dpad.point.y : this.lastInput.y,
     );
 
+    // Camera's current target transform
+    const rect = camera.transform.worldRect();
+
+    // If the camera is approaching a destination (likely one given by this script), do nothing
+    if (!camera.doneTraveling)
+      return;
+
     // Move the camera lead to a point outside the camera's viewframe, unless no input.
     const leadDist = 2;
-    const rect = camera.currentTransform().worldRect();
     this.cameraLead.set(
       rect.center.x + (rect.width/2 + leadDist)*dpad.point.x,
       rect.center.y + (rect.height/2 + leadDist)*dpad.point.y,
@@ -59,11 +66,21 @@ export class ManualMoveCamera extends ControlScript {
 
     this.cameraLead.x = Common.clamp(this.cameraLead.x, 0, max.x);
     this.cameraLead.y = Common.clamp(this.cameraLead.y, 0, max.y);
+
+    // TODO Remove?; I think I've fixed the problem, but I need to test on a more powerful machine.
+    // Debugging, logging block
+    this.log.push(`frame ${Game.frameCount}\n  done travelling? ${camera.doneTraveling}\n  dpad ${dpad.point.toString()},\n  focal ${this.cameraLead.toString()},\n  rect ${rect.toString()}`);
+    if (this.log.length > 120)
+      this.log.shift();
+    if (Game.devController.pressed(Keys.L))
+      console.log(this.log.join('\n'));
   }
+
+  private log: string[] = []; // TODO Remove: debugging stuff
 
   protected disableScript(): void {
     const { camera } = this.assets;
     camera.focalTarget = this.focalSwap;
   }
-  
+
 }
