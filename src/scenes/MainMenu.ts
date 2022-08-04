e7e4970fc0476b6fc3a104e87b314ed57efb934e
Dei Valko <xp.gram@gmail.com>
2022-08-04 14:48:08 -0700

    and temporary visual-please to main menu

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,4 +1,4 @@
-import { PIXI } from "../constants";
+import { PIXI, PixiFilters } from "../constants";
 import { Game } from "..";
 import { VirtualGamepad } from "../scripts/controls/VirtualGamepad";
 import { CommandMenuGUI } from "../scripts/system/gui-menu-components/CommandMenuGUI";
@@ -9,6 +9,10 @@ import { MapData } from "../battle-maps/MapData";
 import { StateMaster } from "../scripts/system/state-management/StateMaster";
 import { MainMenuAssets } from "../scripts/main-menu/MainMenuAssets";
 import { PickMap } from "../scripts/main-menu/PickMap";
+import { Common } from "../scripts/CommonUtils";
+import { Timer } from "../scripts/timer/Timer";
+import { Point } from "pixi.js";
+import { Ease } from "../scripts/Common/EaseMethod";
 
 
 /**
@@ -18,10 +22,18 @@ export class MainMenuScene extends Scene {
 
   private stateMachine!: StateMaster<MainMenuAssets>;
 
-  private gamepad!: VirtualGamepad;
+  private bg = new PIXI.Sprite();
+  private bgTimer!: Timer;
+  private bgCrtFilter = new PixiFilters.CRTFilter({
+    vignetting: 1/4,
+    vignettingAlpha: 1/3,
 
-  private menu!: ListMenu<IconTitle, MapData>;
-  private guiMenu!: CommandMenuGUI<MapData>;
+    curvature: 1/3,
+    lineWidth: 1.0,
+    lineContrast: 1/12,
+
+    noise: 1/96,
+  });
 
   loadStep(): void {
     this.linker.push({name: 'font-map-ui', url: 'assets/font-map-ui.xml'});
@@ -35,10 +47,9 @@ export class MainMenuScene extends Scene {
     this.linker.push({name: 'font-player-splash', url: 'assets/font-player-splash.xml'});
 
     // FIXME Not part of spritesheet
-    this.linker.push({name: 'page-btn-enabled', url: 'assets/tmp-nonbundled/left-page-arrow-enabled.png'});
-    this.linker.push({name: 'page-btn-disabled', url: 'assets/tmp-nonbundled/left-page-arrow-disabled.png'});
-    this.linker.push({name: 'page-icon', url: 'assets/tmp-nonbundled/page-icon.png'});
-    this.linker.push({name: 'page-cur-icon', url: 'assets/tmp-nonbundled/page-current-icon.png'});
+    this.linker.push({name: 'mainmenu-bg1', url: 'assets/tmp-nonbundled/mainmenu-bg1.png'})
+    this.linker.push({name: 'mainmenu-bg2', url: 'assets/tmp-nonbundled/mainmenu-bg2.png'})
+    this.linker.push({name: 'mainmenu-bg3', url: 'assets/tmp-nonbundled/mainmenu-bg3.png'})
   }
 
   setupStep(): void {
@@ -49,14 +60,47 @@ export class MainMenuScene extends Scene {
       entryPoint: PickMap,
     });
 
+    // Set up temporary MainMenu background
+    const center = new Point(
+      Game.display.renderWidth/2,
+      Game.display.renderHeight/2,
+    );
+    const y_displace = 4;
+    const animTime = 6;
+      
+    // const bg_name = Common.choose(['mainmenu-bg1', 'mainmenu-bg2', 'mainmenu-bg3']);
+    const bg_name = 'mainmenu-bg2';
+    this.bg.texture = this.resources[bg_name].texture as PIXI.Texture;
+    this.bg.tint = 0xAAAAAA;  // Darken a little for menu see-ability.
+
+    const newWidth = Game.display.renderWidth;
+    this.bg.height *= newWidth / this.bg.width;
+    this.bg.width = newWidth;
+    this.bg.anchor.set(.5);
+    this.bg.position.set(center.x, center.y - y_displace);
+
+    this.visualLayers.backdrop.filters = [
+      new PIXI.filters.BlurFilter(6),
+      this.bgCrtFilter,
+      new PixiFilters.PixelateFilter(6),  // This doesn't track with viewport resize
+    ];
+
+    this.visualLayers.backdrop.addChild(this.bg);
+    this.bgTimer = Timer
+      .tween(animTime, this.bg, {y: center.y + y_displace}, Ease.sine.inOut)
+      .wait()
+      .tween(animTime, this.bg, {y: center.y - y_displace}, Ease.sine.inOut)
+      .loop();
   }
 
   updateStep(): void {
-
+    if (Game.frameCount % 2 === 0)
+      this.bgCrtFilter.seed = Math.random();
   }
 
   destroyStep(): void {
     this.stateMachine.destroy();
+    this.bgTimer.destroy();
   }
 
 }
